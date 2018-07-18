7663cbb3136f2b38f07d3113cd3105e57d1ec4a8
Daria Kuznetsova <karagioz@yandex.ru>
2018-07-18 23:59:15 +0300

    Wait 10 seconds for an element's appearance

--- test_example.py
+++ test_example.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture()
 def driver(request):
     wd = webdriver.Chrome()
+    wd.implicitly_wait(10)
 
     def fin():
         wd.quit()
