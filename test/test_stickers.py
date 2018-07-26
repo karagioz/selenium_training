57950db96935a66fec57a433eeed0580eeab27a2
Daria Kuznetsova <karagioz@yandex.ru>
2018-07-27 01:05:17 +0300

    Test for stickers presence added

---
+++ test/test_stickers.py
@@ -0,0 +1,6 @@
+def test_stickers(app):
+    app.wd.get("http://localhost/litecart")
+    goods = app.wd.find_elements_by_css_selector("div.image-wrapper")
+    for g in goods:
+        stickers = g.find_elements_by_xpath(".//div[contains(@class, 'sticker')]")
+        assert len(stickers) == 1
