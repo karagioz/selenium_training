1656276269005520f27c4b4bdf729c6c4d376069
Daria Kuznetsova <karagioz@yandex.ru>
2018-08-04 22:19:51 +0300

    Test for creation of a new product added (part 2)

--- test/test_create_product.py
+++ test/test_create_product.py
@@ -1,9 +1,44 @@
 import os.path
+import time
+from selenium.webdriver.support.ui import Select
 
 
 def test_create_product(app):
     app.wd.find_element_by_xpath("//span[text()='Catalog']").click()
     app.wd.find_element_by_xpath("//a[text()=' Add New Product']").click()
+    fill_general_tab(app)
+    open_tab(app, tab_name="Information")
+    fill_information_tab(app)
+    open_tab(app, tab_name="Prices")
+    fill_prices_tab(app)
+    app.wd.find_element_by_xpath("//button[@name='save']").click()
+    app.wd.find_element_by_xpath("//span[text()='Catalog']").click()
+    app.wd.find_element_by_xpath("//a[text()='Rubber Ducks']").click()
+    assert len(app.wd.find_elements_by_xpath("//tr[@class='row']/td/a[text()='Green Frog']")) == 1
+
+
+def open_tab(app, tab_name):
+    app.wd.find_element_by_link_text(tab_name).click()
+    time.sleep(1)
+
+
+def set_select_option(app, select_name, option_text):
+    select_element = Select(app.wd.find_element_by_xpath("//select[@name='%s']" % select_name))
+    select_element.select_by_visible_text('%s' % option_text)
+
+
+def fill_information_tab(app):
+    set_select_option(app, select_name='manufacturer_id', option_text='ACME Corp.')
+    app.wd.find_element_by_xpath("//input[@name='keywords']").send_keys('Fascinating green frog')
+    app.wd.find_element_by_xpath("//input[@name='short_description[en]']"). \
+        send_keys('Fascinating green frog is the best present for everyone')
+    app.wd.find_element_by_css_selector(".trumbowyg-editor"). \
+        send_keys('Fascinating green frog is the best present for everyone')
+    app.wd.find_element_by_xpath("//input[@name='head_title[en]']").send_keys('Frog')
+    app.wd.find_element_by_xpath("//input[@name='meta_description[en]']").send_keys('Frog')
+
+
+def fill_general_tab(app):
     app.wd.find_element_by_xpath("//input[@name='status' and @value='1']").click()
     app.wd.find_element_by_xpath("//input[@name='name[en]']").send_keys('Green Frog')
     app.wd.find_element_by_xpath("//input[@name='code']").send_keys('green_frog')
@@ -17,4 +52,13 @@ def test_create_product(app):
     app.wd.find_element_by_xpath("//input[@name='new_images[]']").send_keys(filename)
     app.wd.find_element_by_xpath("//input[@name='date_valid_from']").send_keys('01.08.2018')
     app.wd.find_element_by_xpath("//input[@name='date_valid_to']").send_keys('31.08.2018')
-    app.wd.find_element_by_xpath("//button[@name='save']").click()
+
+
+def fill_prices_tab(app):
+    app.wd.find_element_by_xpath("//input[@name='purchase_price']").clear()
+    app.wd.find_element_by_xpath("//input[@name='purchase_price']").send_keys('23')
+    set_select_option(app, select_name='purchase_price_currency_code', option_text='US Dollars')
+    app.wd.find_element_by_xpath("//input[@name='prices[USD]']").send_keys('23')
+    app.wd.find_element_by_xpath("//input[@name='gross_prices[USD]']").send_keys('23')
+    app.wd.find_element_by_xpath("//input[@name='prices[EUR]']").send_keys('22')
+    app.wd.find_element_by_xpath("//input[@name='gross_prices[EUR]']").send_keys('22')
