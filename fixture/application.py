a26c5f0f75942fbe1848d67b20df52954f499815
Daria Kuznetsova <karagioz@yandex.ru>
2018-08-19 23:41:32 +0300

    Redirect traffic to a proxy server added

--- fixture/application.py
+++ fixture/application.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.general import GeneralHelper
+# from browsermobproxy import Server
 
 
 class Application:
 
     def __init__(self, browser, base_url):
+        # self.server = Server("C:\\Tools\\browsermob-proxy-2.1.4\\bin\\browsermob-proxy")
+        # self.server.start()
+        # self.proxy = self.server.create_proxy()
         if browser == "firefox":
             self.wd = webdriver.Firefox(capabilities={"marionette": False})
         elif browser == "firefox_nightly":
             self.wd = webdriver.Firefox(firefox_binary="C:\\Program Files\\Firefox Nightly\\firefox.exe")
         elif browser == "chrome":
-            self.wd = webdriver.Chrome(desired_capabilities={"chromeOptions": {"args": ["start-maximized"]},
-                                                             "loggingPrefs": {'performance': 'ALL'}})
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--start-maximized')
+            # chrome_options.add_argument("--proxy-server={0}".format(self.proxy.proxy))
+            self.wd = webdriver.Chrome(chrome_options=chrome_options)
+                                       # , desired_capabilities={"loggingPrefs": {'performance': 'ALL'}})
         elif browser == "ie":
             self.wd = webdriver.Ie()
         else:
@@ -34,4 +41,5 @@ class Application:
         wd.get(self.base_url)
 
     def destroy(self):
+        # self.server.stop()
         self.wd.quit()
--- test/test_login.py
+++ test/test_login.py
@@ -1,6 +1,9 @@
 def test_login(app):
+    # app.proxy.new_har("test proxy")
     app.session.logout()
     username = "admin"
     password = "admin"
     app.session.login(username=username, password=password)
     assert ("You are now logged in as %s" % username in app.wd.page_source)
+    # for l in app.proxy.har["log"]["entries"]:
+    #     print(l["request"]["url"] + ' : ' + str(l["response"]["status"]))
